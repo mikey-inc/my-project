c5f4a077edc43e8dae0b65c0428e8670e9e2a7cb
Mikey Inc <jenkinsCImikeyInc@gmail.com>
2014-03-03 21:05:38 +0530

    Update BookController.groovy

--- grails-app/controllers/org/example/BookController.groovy
+++ grails-app/controllers/org/example/BookController.groovy
@@ -3,7 +3,7 @@ package org.example
 import aa.bb.Hello
 
 class BookController {
-    def scaffold = Book // Note the capital "B"
+   // def scaffold = Book // Note the capital "B"
 	Hello hello = new Hello()
 	
 	def index(){
