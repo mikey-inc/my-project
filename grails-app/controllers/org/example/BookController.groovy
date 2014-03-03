76ac36f3d9bfbd6fcc2e9baee11e563d8879ceca
Ranjit Sahoo <ran89here@gmail.com>
2014-03-03 19:45:36 +0530

    first commit

--- grails-app/controllers/org/example/BookController.groovy
+++ grails-app/controllers/org/example/BookController.groovy
@@ -1,4 +1,13 @@
 package org.example
+
+import aa.bb.Hello
+
 class BookController {
     def scaffold = Book // Note the capital "B"
+	Hello hello = new Hello()
+	
+	def index(){
+			hello.msg()
+	}
+	
 }
